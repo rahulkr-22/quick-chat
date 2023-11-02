1f6c90a16cc76f1de631ed71741b393a4f6d9eee
Rahul Kumar <rahulpanjwara57@gmail.com>
2023-11-02 21:38:39 +0530

    commit

---
+++ frontend/src/index.js
@@ -0,0 +1,20 @@
+import React, { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
+import ChatProvider from "./Context/ChatProvider";
+import { BrowserRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <StrictMode>
+    <ChakraProvider>
+      <BrowserRouter>
+        <ChatProvider>
+          <App />
+        </ChatProvider>
+      </BrowserRouter>
+    </ChakraProvider>
+  </StrictMode>,
+  document.getElementById("root")
+);
